31613caf8aee5f8dba97e644e5cb9055e07cc77b
Shaik Sumayya Tabasum <shaiksumayya596@gmail.com>
2026-04-24 01:02:08 +0530

    Update import paths for components in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import Navbar from "../components/Navbar";
-import Hero from "../components/Hero";
-import Features from "../components/Features";
-import Growthsection from "../components/Growthsection";
-import Footer from "../components/Footer";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Growthsection from "./components/Growthsection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
